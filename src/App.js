dd90ebc1d44c04639ed9d02d777a38caf0a22c20
Fabricio Baeza <fabriciobaeza@gmail.com>
2022-01-03 23:59:19 -0500

    setInterval

--- src/App.js
+++ src/App.js
@@ -14,6 +14,16 @@ const App = () => {
 
   const [currentColorArrangement, setCurrentColorArrangement] = useState([])
 
+  const checkForColumnOfThree = () => {
+    for(let i = 0; i < 47; i++){
+      const columnOfThree = [i, i +width, i + width * 2]
+      const decidedColor = currentColorArrangement[i]
+      if(columnOfThree.every(square => currentColorArrangement[square] === decidedColor)){
+        columnOfThree.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
+
   const createBoard = () => {
     const randomColorArrangement = []
     for (let i = 0; i < width * width; i++){
@@ -27,11 +37,22 @@ const App = () => {
     createBoard()
   }, [])
 
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkForColumnOfThree()
+    }, 100)
+    return () => clearInterval(timer)
+  },[checkForColumnOfThree])
+
   return (
     <div className="app">
       <div className="game">
         {currentColorArrangement.map((candyColor, index) => (
-          <img key={index} style={{backgroundColor: candyColor}}/>
+          <img 
+          key={index} 
+          style={{backgroundColor: candyColor}}
+          alt={candyColor}
+          />
         ))}
       </div>
     </div>
